42db00ecfacd78c8768bcc45a9b76666844aa4b8
Moh Hasbi Assidiqi <mohhasbias@gmail.com>
2014-10-25 21:43:31 +0700

    extract javascript from index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -165,7 +165,7 @@ module.exports = function (grunt) {
       app: {
         ignorePath: /^\/|\.\.\//,
         src: ['<%= config.app %>/index.html'],
-        exclude: ['bower_components/bootstrap/dist/js/bootstrap.js']
+        exclude: []
       }
     },
 
